a2da796c16f6e49f41ef328a72a55af289880e70
ylcnarslan <ylcnarslan@windowslive.com>
2012-12-13 19:21:54 +0200

    Revert "nnn"
    This reverts commit ccc5d0545d02f255566fad6ff2fec4014965a276.

--- src/main/java/EmailTest.java
+++ src/main/java/EmailTest.java
@@ -29,8 +29,6 @@ public class EmailTest {
 							MimeMessageHelper message = new MimeMessageHelper(
 									mimeMessage);
 							// mail sending parameters
-							
-							
 							message.setTo(address);
 							message.setFrom("davet@kimegitsem.com");
 							message.setSubject("sana kimegitsem?com’dan arkadaslik istegi gonderdi");
