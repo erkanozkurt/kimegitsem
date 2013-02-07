aace45e918952d1afd7d5ffe84c0f3281ec76caf
ylcnarslan <ylcnarslan@windowslive.com>
2013-02-07 21:37:30 +0200

    kategori ağacı 2

---
+++ src/main/java/com/persona/kg/common/StatConstants.java
@@ -0,0 +1,21 @@
+package com.persona.kg.common;
+
+public class StatConstants {
+	public static short AT_UNDEFINED=0;
+	public static short AT_COMMENT=1;
+	public static short AT_COMPLIMENT=2;
+	public static short AT_CHECKIN=3;
+	public static short AT_LIKE=4;
+	public static short AT_VIEW=5;
+	public static short AT_SUGGEST=6;
+	public static short AT_REQUEST_SUGGESTION=7;
+	public static short AT_REGISTRATION=8;
+	
+	
+	public static short IT_UNDEFINED=0;
+	public static short IT_SUBSCRIBER=1;
+	public static short IT_POI=2;
+	public static short IT_PICTURE=3;
+	
+	
+}
